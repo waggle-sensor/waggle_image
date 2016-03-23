120dfc586c2d97945a79aee2383054e48e732047
Wolfgang Gerlach <wgerlach@users.noreply.github.com>
2016-03-23 16:37:13 -0500

    delete files first

--- device_rules/wwan_modems/install.sh
+++ device_rules/wwan_modems/install.sh
@@ -5,7 +5,8 @@ set -e
 rm -f /etc/udev/rules.d/75-wwan-net.rules
 cp 75-wwan-net.rules /etc/udev/rules.d/
 
-cp ./wvdial.conf to /etc/
+rm -f /etc/wvdial.conf
+cp ./wvdial.conf /etc/
 
 set +x
 echo "run: udevadm control --reload-rules"

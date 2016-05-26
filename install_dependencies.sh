0e2eabf3d07edc6a3c718f3dae4e59cb7e9fda66
Wolfgang Gerlach <wgerlach@users.noreply.github.com>
2016-05-26 10:37:20 -0500

    clean up to enable creation of eMMC image

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -4,5 +4,6 @@ set -e
 set -x
 
 
-#nothing here anymore
+apt-get install -y htop iotop iftop bwm-ng screen git python-dev python-serial python-pip tree psmisc wvdial dosfstools autossh parted bash-completion
+
 

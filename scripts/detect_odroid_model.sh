c3a51cbe5059aaa4edbc6e83ee200bd5a38bfeeb
Wolfgang Gerlach <wgerlach@users.noreply.github.com>
2016-05-27 15:57:41 -0500

    use /proc/cpuinfo

--- scripts/detect_odroid_model.sh
+++ scripts/detect_odroid_model.sh
@@ -1,23 +1,50 @@
 #!/bin/bash
 
 
-HEADER=`head -n 1 /media/boot/boot.ini`
 
+MODEL_REPORTED=$(cat /proc/cpuinfo | grep Hardware | grep -o "[^ ]*$")
 
-ODROID_MODEL=""
-
-if [ ${HEADER}x == "ODROIDXU-UBOOT-CONFIGx" ] ; then
-  if [ -e /media/boot/exynos5422-odroidxu3.dtb ] ; then
-    # XU3 and XU4 are identical
-    ODROID_MODEL ="XU3"
-  fi
-elif [ ${HEADER}x == "ODROIDC-UBOOT-CONFIGx" ] ; then
-  ODROID_MODEL ="C"
+if [ "${MODEL_REPORTED}x" == "x" ] ; then
+  echo "no model detected"
+  exit 1
 fi
 
-if [ ${ODROID_MODEL}x == "x" ] ; then
-  echo "Device not recognized"
+
+if [ "${MODEL_REPORTED}x" == "ODROIDCx" ] ; then
+  ODROID_MODEL="C"
+elif [ "${MODEL_REPORTED}x" == "ODROID-XU3x" ] ; then
+  ODROID_MODEL="XU3"
+else
+  echo "Model ${MODEL_REPORTED} unknown."
   exit 1
-fi 
+fi
 
 echo "ODROID_MODEL=${ODROID_MODEL}"
+
+
+exit 0
+
+
+####################################
+# below is the old code that was used to detect the hardware model. We keep it here just in case we may have to use it again.
+
+#HEADER=`head -n 1 /media/boot/boot.ini`
+
+
+#ODROID_MODEL=""
+
+#if [ ${HEADER}x == "ODROIDXU-UBOOT-CONFIGx" ] ; then
+#  if [ -e /media/boot/exynos5422-odroidxu3.dtb ] ; then
+#    # XU3 and XU4 are identical
+#    ODROID_MODEL ="XU3"
+#  fi
+#elif [ ${HEADER}x == "ODROIDC-UBOOT-CONFIGx" ] ; then
+#  ODROID_MODEL ="C"
+#fi
+
+#if [ ${ODROID_MODEL}x == "x" ] ; then
+#  echo "Device not recognized"
+#  exit 1
+#fi 
+
+#echo "ODROID_MODEL=${ODROID_MODEL}"

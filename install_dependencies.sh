90c2a62064944130c021983de7aa387fe91086ed
Wolfgang Gerlach <wgerlach@users.noreply.github.com>
2016-06-28 11:27:42 -0500

    add fswebcam v4l-utils

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -4,6 +4,6 @@ set -e
 set -x
 
 
-apt-get install -y htop iotop iftop bwm-ng screen git python-dev python-serial python-pip tree psmisc wvdial dosfstools autossh parted bash-completion
+apt-get install -y htop iotop iftop bwm-ng screen git python-dev python-serial python-pip tree psmisc wvdial dosfstools autossh parted bash-completion fswebcam v4l-utils
 
 

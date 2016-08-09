57ef490b32cfd0ad1d7b06c3012bf3af8f3524d2
MuffinSpawn <muffinspawn@gmail.com>
2016-08-09 15:10:55 -0500

    Simplified heartbeat dead man switch logic to simply stat the alive file every cycle.

--- scripts/heartbeat.sh
+++ scripts/heartbeat.sh
@@ -109,25 +109,15 @@ set +x
 echo "Starting heartbeat..."
 
 
-
-set +x
-
-LAST_TIME=`date +%s`
 while [ 1 ] ; do 
   PIN_HIGH=1
   if [ ${DEVICE}x == "Cx" ] ; then
     CURRENT_TIME=`date +%s`
-    #CHECK_DURATION=`expr ${CURRENT_TIME} - ${LAST_TIME}`
-    CHECK_DURATION=`python -c "print(${CURRENT_TIME} - ${LAST_TIME})"`
-    if [ ${CHECK_DURATION} -ge 30 ]; then
-      LAST_TIME=${CURRENT_TIME}
-      ALIVE_TIME=`stat -c %Y /usr/lib/waggle/alive`
-      #ALIVE_DURATION=`expr ${CURRENT_TIME} - ${ALIVE_TIME}`
-      ALIVE_DURATION=`python -c "print(${CURRENT_TIME} - ${ALIVE_TIME})"`
-      if [ ${ALIVE_DURATION} -gt 60 ]; then
-        # Skip this heartbeat
-        PIN_HIGH=0
-      fi
+    ALIVE_TIME=`stat -c %Y /usr/lib/waggle/alive`
+    ALIVE_DURATION=`python -c "print(${CURRENT_TIME} - ${ALIVE_TIME})"`
+    if [ ${ALIVE_DURATION} -gt 10 ]; then
+      # Skip this heartbeat
+      PIN_HIGH=0
     fi
   fi
   echo ${PIN_HIGH} > /sys/class/gpio/gpio${GPIO_EXPORT}/value

e06ff1cac04062ab3c1c02528329eb2b3b7b12a9
Rajesh Sankaran <rajesh@anl.gov>
2016-08-11 20:39:32 -0500

    simplified WWAN files, now more everything is looking up.

--- device_rules/wwan_modems/wvwaggle.sh
+++ device_rules/wwan_modems/wvwaggle.sh
@@ -2,6 +2,9 @@
 #This goes in /usr/bin/wvwaggle.sh, and make the file executable with chmod +x /usr/bin/wvwaggle.sh
 #Let us give the modem time to settle.
 sleep 20
-rm /dev/$1
-ln -s  $(ls /dev/$1* | sort | head -1) /dev/$1
-wvdial $1
+rm /dev/attwwan
+sleep 2
+ln -s  $(ls /dev/attwwan[0-9] | sort | head -1) /dev/attwwan
+sleep 2
+wvdial
+

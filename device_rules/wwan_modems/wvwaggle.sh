d217a3cad00403b0d8a472d22da6bb0efe65b88b
Peter Lane <lane@uchicago.edu>
2016-09-02 12:37:38 -0500

    Change sleep time in wvwaggle.sh from 20 to 7 seconds.

--- device_rules/wwan_modems/wvwaggle.sh
+++ device_rules/wwan_modems/wvwaggle.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #This goes in /usr/bin/wvwaggle.sh, and make the file executable with chmod +x /usr/bin/wvwaggle.sh
 #Let us give the modem time to settle.
-sleep 20
+sleep 7
 rm /dev/attwwan
 sleep 2
 ln -s  $(ls /dev/attwwan[0-9] | sort | head -1) /dev/attwwan

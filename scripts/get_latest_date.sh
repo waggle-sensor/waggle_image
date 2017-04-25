83070b0962016a81484d0687ab80b17382d08b23
Peter Lane <muffinspawn@gmail.com>
2017-04-24 20:21:20 -0500

    select latest base image date from branch name

--- scripts/get_latest_date.sh
+++ scripts/get_latest_date.sh
@@ -3,7 +3,10 @@
 declare -r latest_url="http://www.mcs.anl.gov/research/projects/waggle/downloads/waggle_images/base/latest.txt"
 declare -r branch=$(git branch)
 declare -A dates
-dates[v2.5.1]=20170307
+dates=([develop]=20170307 [v2.5.1]=20170307)
 date=${dates[$branch]}
-echo $date
-curl ${latest_url} 2>&1 | sed -n 's/..*\([0-9]\{8\}\).img.xz/\1/p'
+if [ "x$date" == "x" ]; then
+  curl ${latest_url} 2>&1 | sed -n 's/..*\([0-9]\{8\}\).img.xz/\1/p'
+else
+  echo $date
+fi

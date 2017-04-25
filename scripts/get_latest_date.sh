476bf112c2afbf5965b97eb670ecfead6cdfb7d8
Peter Lane <muffinspawn@gmail.com>
2017-04-24 20:29:20 -0500

    update date of base image

--- scripts/get_latest_date.sh
+++ scripts/get_latest_date.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
 
 declare -r latest_url="http://www.mcs.anl.gov/research/projects/waggle/downloads/waggle_images/base/latest.txt"
-curl ${latest_url} 2>&1 | sed -n 's/..*\([0-9]\{8\}\).img.xz/\1/p'
+declare -r branch=$(git branch | awk '/^\*/ { sub(/^\* /, ""); print}')
+declare -A dates
+dates=([develop]=20170307 [v2.5.1]=20170307 ["hotfix-2.5.2"]=20170425)
+date=${dates[$branch]}
+if [ "x$date" == "x" ]; then
+  curl ${latest_url} 2>&1 | sed -n 's/..*\([0-9]\{8\}\).img.xz/\1/p'
+else
+  echo $date
+fi
+

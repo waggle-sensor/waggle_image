f92fc9e27f71d90d3ab89bf5e71c846b38893072
Peter Lane <muffinspawn@gmail.com>
2017-08-05 21:26:38 -0500

    Don't try to setup the RabbitMQ APT repository if we don't need to. Also try to wget the .asc file if it is needed but doesn't exist.

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -35,10 +35,15 @@ declare -r script_dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
 export LC_ALL=C
 
-echo 'deb http://www.rabbitmq.com/debian/ testing main' | tee /etc/apt/sources.list.d/rabbitmq.list
-apt-key add /root/rabbitmq-release-signing-key.asc
-# wget -O- https://www.rabbitmq.com/rabbitmq-release-signing-key.asc | apt-key add -
-
+echo ${apt_packages[*]} | grep rabbitmq
+if [ $? -eq 0 ]; then
+  echo 'deb http://www.rabbitmq.com/debian/ testing main' | tee /etc/apt/sources.list.d/rabbitmq.list
+  if [ -e /root/rabbitmq-release-signing-key.asc ]; then
+    apt-key add /root/rabbitmq-release-signing-key.asc
+  else
+    wget -O- https://www.rabbitmq.com/rabbitmq-release-signing-key.asc | apt-key add -
+  fi
+fi
 
 apt-get update
 apt-key update

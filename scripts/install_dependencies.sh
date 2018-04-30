3220a1d8be94b0ec556963a118782753ffb9df3c
Yongho Kim <ironyonghokim@gmail.com>
2018-04-30 10:17:15 -0500

    Pip install 9.0.3 as 10.0.1 has an issue

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -66,7 +66,7 @@ if [ "x" == "x${python2_packages[*]}" ]; then
   echo "No Python 2 packages specified. Skipping pip operation."
 else
   echo "Installing the following Python 2 packages: ${python2_packages[@]}"
-  pip install --upgrade pip
+  pip install --upgrade pip==9.0.3
   pip install ${python2_packages[@]}
 fi
 
@@ -76,7 +76,7 @@ if [ "x" == "x${python3_packages[*]}" ]; then
   echo "No Python 3 packages specified. Skipping pip3 operation."
 else
   echo "Installing the following Python 3 packages: ${python3_packages[@]}"
-  pip3 install --upgrade pip
+  pip3 install --upgrade pip==9.0.3
   cd ${script_dir}/../var/cache/pip3/archives
   pip3 install ${python3_packages[@]}
 fi

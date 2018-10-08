b85f893459ccd810838c14b6dc5c297a9a709b1e
Rajesh Sankaran <rajeshxsankaran@users.noreply.github.com>
2018-10-08 17:53:29 -0500

    Update install_dependencies.sh

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -68,7 +68,8 @@ if [ "x" == "x${python2_packages[*]}" ]; then
 else
   echo "Installing the following Python 2 packages: ${python2_packages[@]}"
   #pip install --upgrade pip==9.0.3
-  pip --no-cache-dir install --upgrade pip
+  #pip --no-cache-dir install --upgrade pip
+  pip --no-cache-dir install --upgrade pip==10.0.1
   pip --no-cache-dir install ${python2_packages[@]}
 fi
 
@@ -78,7 +79,8 @@ if [ "x" == "x${python3_packages[*]}" ]; then
   echo "No Python 3 packages specified. Skipping pip3 operation."
 else
   echo "Installing the following Python 3 packages: ${python3_packages[@]}"
-  pip3 --no-cache-dir install --upgrade pip==9.0.3
+  #pip3 --no-cache-dir install --upgrade pip==9.0.3
+  pip3 --no-cache-dir install --upgrade pip==10.0.1
   #pip3 --no-cache-dir install --upgrade pip
   cd ${script_dir}/../var/cache/pip3/archives
   pip3 --no-cache-dir install ${python3_packages[@]}

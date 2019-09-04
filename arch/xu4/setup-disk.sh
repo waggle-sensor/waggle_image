1c07bd6655ac72e67a1afff0645456e43aa7a075
Sean Shahkarami <sean.shahkarami@gmail.com>
2019-09-04 13:44:00 -0500

    added arch setup scripts

---
+++ arch/xu4/setup-disk.sh
@@ -0,0 +1,56 @@
+#!/bin/bash -u
+
+log() {
+    echo -e "\033[34m$(date +'%Y/%m/%d %H:%M:%S') - $*\033[0m"
+}
+
+fatal() {
+    >&2 echo -e "\033[31m$(date +'%Y/%m/%d %H:%M:%S') - $*\033[0m"
+    exit 1
+}
+
+disk="$1"
+
+if test -e ArchLinuxARM-odroid-xu3-latest.tar.gz; then
+        log using cached image
+else
+        log pulling image
+        wget http://os.archlinuxarm.org/os/ArchLinuxARM-odroid-xu3-latest.tar.gz
+fi
+
+dd if=/dev/zero of=$disk bs=1M count=8
+
+log creating partitions
+fdisk $disk <<EOF
+o
+n
+p
+1
+4096
+
+w
+EOF
+
+log creating filesystems
+mkfs.ext4 "$disk"1
+
+mkdir -p root
+umount root
+mount "$disk"1 root
+
+log unpacking image
+bsdtar -xpf ArchLinuxARM-odroid-xu3-latest.tar.gz -C root
+
+log cleaning partitions
+rm root/etc/resolv.conf
+
+log writing bootloader
+(cd root/boot; sh sd_fusing.sh "$disk")
+
+systemd-nspawn -D root -P bash -s <<EOF
+pacman-key --init
+pacman-key --populate archlinuxarm
+pacman -Syy
+EOF
+
+umount root

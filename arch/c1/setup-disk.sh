8ae89ced7ab8f6c19fc9474443be785b6f61aa24
Sean Shahkarami <sean.shahkarami@gmail.com>
2019-09-17 16:26:51 -0500

    fixed bind

--- arch/c1/setup-disk.sh
+++ arch/c1/setup-disk.sh
@@ -104,9 +104,16 @@ ssh-keygen -N '' -f /etc/ssh/ssh_host_ecdsa_key -t ecdsa -b 521
 ssh-keygen -N '' -f /etc/ssh/ssh_host_ed25519_key -t ed25519
 ssh-keygen -N '' -f /etc/ssh/ssh_host_rsa_key -t rsa -b 2048
 
-# prepare for bind mounts
-mkdir -p /var/lib /var/log /var/tmp /etc/docker
-mkdir -p /wagglerw/var/lib /wagglerw/var/log /wagglerw/var/tmp /wagglerw/etc/docker
+# prepare bind mounts
+for d in /var/lib /var/log /var/tmp /etc/docker; do
+    mkdir -p $d
+    mkdir -p /wagglerw/$d
+done
+
+for f in /etc/hostname; do
+    touch $f
+    touch /wagglerw/$f
+done
 EOF
 
 cat <<EOF > $rootmount/etc/fstab
